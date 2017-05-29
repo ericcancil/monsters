97d4860c3554d67c341c0949eabb444a73ccdda1
Eric Cancil <ericcancil@cotahealthcare.com>
2017-05-29 15:30:03 -0400

    Initial Commit

---
+++ macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Monsters
+//
+//  Created by Eric Cancil on 5/29/17.
+//  Copyright © 2017 Eric Cancil. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ macOS/GameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GameViewController.swift
+//  macOS
+//
+//  Created by Eric Cancil on 5/29/17.
+//  Copyright © 2017 Eric Cancil. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
